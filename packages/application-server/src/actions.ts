b7576052ab8429a2a2a7b4fa719fa0379753a774
kadhirvelm <kadhirvelm@berkeley.edu>
2020-01-05 21:12:18 +0000

    Fix @types/node

--- packages/application-server/src/actions.ts
+++ packages/application-server/src/actions.ts
@@ -1,4 +1,3 @@
-import { pingTenTimes } from "@looking-glass/application-scripts";
 import { BasicAction, RendererListener } from "./classes";
 import { IPing, IPingStatus } from "./typings";
 
@@ -18,12 +17,6 @@ const START_PING = new BasicAction<IPing>(CHANNELS.START_PING, event => {
     PING_STATUS.channel,
     PING_STATUS.verifyArgs({ isPinging: true })
   );
-  pingTenTimes().then(() => {
-    event.sender.send(
-      PING_STATUS.channel,
-      PING_STATUS.verifyArgs({ isPinging: false })
-    );
-  });
 });
 
 const STOP_PING = new BasicAction<IPing>(CHANNELS.STOP_PING, event => {
