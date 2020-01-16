315ce24ddafd789c60d132a069c4ee48f3473222
kadhirvelm <kadhirvelm@berkeley.edu>
2020-01-16 22:02:06 +0000

    Revamp clean lock and fix lock

--- scripts/cleanLock.js
+++ scripts/cleanLock.js
@@ -1,10 +1,37 @@
 const execSync = require("child_process").execSync;
 
-const output = execSync("npx yarn-deduplicate --list yarn.lock").toString();
+function testBasicCleanLock() {
+  const output = execSync("npx yarn-deduplicate --list yarn.lock").toString();
 
-if (output === "") {
-  process.exit(0);
-} else {
-  console.log("The lockfile is not clean, please run 'npx yarn-deduplicate yarn.lock' in the root directory.");
-  process.exit(1);
+  if (output === "") {
+    return;
+  }
+
+  console.log("The lockfile is not clean, please run 'yarn clean:lock' in the root directory.");
+  throw new Error("The lockfile is not clean.");
 }
+
+function runYarnAndTestForChanges() {
+  execSync("yarn", { stdio: "ignore" });
+  const output = execSync("git diff yarn.lock").toString();
+  if (output === "") {
+    return;
+  }
+
+  console.log(output);
+
+  console.log("The lockfile has changed, please run 'yarn' in the root folder and commit the changes.");
+  throw new Error("The lockfile needs changes.");
+}
+
+function main() {
+  try {
+    runYarnAndTestForChanges();
+    testBasicCleanLock();
+    process.exit(0);
+  } catch {
+    process.exit(1);
+  }
+}
+
+main();
